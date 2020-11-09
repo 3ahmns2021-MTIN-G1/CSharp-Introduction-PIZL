2fdac0207061e0bc22a2646736a61314ecda6434
Linda Pizzato <linda.pizzato03@htl-salzburg.ac.at>
2020-11-09 14:11:06 +0100

    Add Scripts Folder and ExampleClassScript

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,21 @@
+﻿// Bibliotheken/Asembly Refernce/Namespace
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Deklaration einer Klasse
+
+// public            -> access modifier (Zugriffsrechte); keyword (vorreservierte Schlüsselwörter)
+// class             -> keyword zur Deklaration einer Klasse
+// ExampleClass      -> identifier (Name der Klasse)
+// :MonoBehaivour    -> erstellen einer Unterklasse von MonoBehaivour
+// {}                -> Scope/Wirkungsbereich (Was ist alles in der Klasse enthalten
+
+// Klasse:  Säugetiere  -> Unterklasse: Hund
+//                      -> Unterklasse: Katze
+
+public class ExampleClass : MonoBehaviour
+{
+
+}
