738c15a527475e1737cf6b3664a932d4233fa15a
Linda Pizzato <linda.pizzato03@htl-salzburg.ac.at>
2020-11-23 14:19:45 +0100

    Add Wiederholung and Uebung01

---
+++ Assets/Scripts/Master.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Master : MonoBehaviour
+{
+    public float x;
+    public Rigidbody rigidBody;
+    public bool boolVar;
+    public BoxCollider boxCollider;
+    public float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody.mass = x+20; // eingestellter Wert + 20 
+        rigidBody.useGravity = boolVar;
+        rigidBody.isKinematic = boolVar;
+        boxCollider.isTrigger = boolVar;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.angularDrag = x+50;
+        // rigidBody.drag = x - y;
+        rigidBody.drag = x - rigidBody.mass;
+    }
+}
---
+++ Assets/Scripts/Wiederholung.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiederholung : MonoBehaviour
+{
+    public Canvas canvasVariable;
+    public Vector3 vectorVariable;
+    public Transform transVariable;
+    public Wiederholung whVariable;
+    public float xValue;
+    public Vector3 testVec = new Vector3(3, 3, 5);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xValue = whVariable.transVariable.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
